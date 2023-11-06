1ee5fba767ce98ee49ba390b7087318dff78a1d5
shaba-imran <imranusmanshaba@gmail.com>
2023-11-06 10:27:20 +0100

    Chore: add embed videos for shortclips route

--- src/routes/ShortClips.js
+++ src/routes/ShortClips.js
@@ -3,10 +3,22 @@ import YoutubeEmbed from "../components/YoutubeEmbed/YoutubeEmbed";
 const ShortClips = () => {
   return (
     <div>
-      <YoutubeEmbed src={""} title={""} />
-      <YoutubeEmbed src={""} title={""} />
-      <YoutubeEmbed src={""} title={""} />
-      <YoutubeEmbed src={""} title={""} />
+      <YoutubeEmbed
+        src={"https://www.youtube.com/embed/ggFKLxAQBbc?si=eXOJ4RrK4axhn-og"}
+        title={"The Matrix: Dodge this"}
+      />
+      <YoutubeEmbed
+        src={"https://www.youtube.com/embed/zE7PKRjrid4?si=aWSAlGapoOc1czLQ"}
+        title={"Blue Pill or Red Pill - The Matrix"}
+      />
+      <YoutubeEmbed
+        src={"https://www.youtube.com/embed/J4072lFVPw8?si=S-x2B-QsCWpfbu4A"}
+        title={"The Chase: Enter the Trinity | The Matrix Reloaded"}
+      />
+      <YoutubeEmbed
+        src={"https://www.youtube.com/embed/Z2eCmhBgsyI?si=nhjBbxbA9BNi0Ipn"}
+        title={"Shootout in the lobby | The Matrix"}
+      />
     </div>
   );
 };
