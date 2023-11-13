1e4340b027d8fdb2cebb22f6a5ac100c460667ad
shaba-imran <imranusmanshaba@gmail.com>
2023-11-13 10:19:07 +0100

    Chore: add a basename

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename="/The-Matrix">
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
